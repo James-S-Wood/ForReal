2235f4fb0856da49a0a70f7c2990eb67a133434f
jameswood <jswood@live.com>
2022-11-24 13:47:25 -0500

    Addition of print lines

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
+
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+            int a = 5;
+            int b = 10;
+            int c = 15;
+            int d = 20;
+            int newValue = a + b + c + d;
+        System.out.println(newValue);
+        System.out.println("The Value is " + newValue);
+        System.out.println("The Value is " + newValue + " and the value is " + newValue);
+        System.out.println("The Value is " + newValue + " and the value is " + newValue + " and the value is " + newValue);
+        System.out.println("The Value is " + newValue + " and the value is " + newValue + " and the value is " + newValue + " and the value is " + newValue);
+
+            int myInt = 5;
+            int myInt2 = 10;
+            int myInt3 = 15;
+
+        System.out.println( myInt + myInt2 + myInt3);
+        System.out.println( myInt + myInt2 + myInt3 + " is the value");
+        System.out.println( "The value is " + myInt + myInt2 + myInt3);
+        System.out.println( "The value is " + (myInt + myInt2 + myInt3));
+
+
     }
 }
