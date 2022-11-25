688ca0c54b5a361229c442f8edba7c03bac90d53
jameswood <jswood@live.com>
2022-11-25 10:22:50 -0500

    Addition of if/else statement

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,19 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+
             int a = 5;
             int b = 10;
             int c = 15;
             int d = 20;
             int newValue = a + b + c + d;
+
+            if (50 == newValue) {
+                System.out.println("The value is 50");
+            } else {
+                System.out.println("The value is not 50");
+            }
+
         System.out.println(newValue);
         System.out.println("The Value is " + newValue);
         System.out.println("The Value is " + newValue + " and the value is " + newValue);
